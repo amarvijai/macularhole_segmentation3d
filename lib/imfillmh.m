22c6211940b60ff3f182600f2b52ee47ed17bbb7
Amar Nasrulloh <amar.v.nasrulloh@dur.ac.uk>
2019-04-10 15:18:39 +0100

    clean up

--- lib/imfillmh.m
+++
@@ -1,8 +0,0 @@
-function im = imfillmh(im)
-%% filter
-    cc = bwconncomp(im);
-    im = bwareaopen(im, max(cellfun(@length, cc.PixelIdxList)));
-    cc = bwconncomp(~im);
-    im = ~bwareaopen(~im, max(cellfun(@length, cc.PixelIdxList)));
-end
-
--- lib/mldangleplane.m
+++
@@ -1,9 +0,0 @@
-function [mldelevationangle1,mldelevationangle2] = mldangleplane(mldplane)
-    bsxy = squeeze(max(mldplane,[],3));
-    bsxy = bwmorph(bsxy,'bridge');
-    [mldelevationangle1] = mldelevation(bsxy);
-    bszy = squeeze(max(mldplane,[],2));
-    bszy = bwmorph(bszy,'bridge');
-    [mldelevationangle2] = mldelevation(bszy);
-end
-
